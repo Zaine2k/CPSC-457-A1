db3c9e5dc55f99a872b221f626b011eb329a38a4
Zaine2k <165634822+Zaine2k@users.noreply.github.com>
2026-09-20 01:11:09 -0600

    Filename change, added basic assignment structure

---
+++ a1.c
@@ -0,0 +1,17 @@
+/*
+Name: Zaine Ancheta
+UCID: 30214484
+Tutorial: T14
+TA: Zahra Arabi
+
+Assignment 1
+*/
+
+#include <stdlib.h> // Standard header file for C
+#include <stdio.h> // Header file for printf function
+#include <sys/types.h> // Header file for process IDs (PID)
+
+int main(void)
+{
+    return 0;
+}
